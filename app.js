b168ded949b730ca694338d4f51ae535bc6378fa
siddhantkhariwal <siddhant.209301640@muj.manipal.edu>
2023-02-22 03:33:51 +0530

    updating the compose page

--- app.js
+++ app.js
@@ -28,8 +28,17 @@ app.get("/contact",function(req,res){
   res.render("contact",{contact : contactContent});
 })
 
+app.get("/compose",function(req,res){
+  res.render("compose");
+})
 
-
+app.post("/compose",function(req,res){
+  const post = {
+    title : req.body.postTitle,
+    content : req.body.postBody
+  };
+  console.log(req.body.postTitle);
+})
 
 
 
